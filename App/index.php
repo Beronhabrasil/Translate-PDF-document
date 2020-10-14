ebe8fde0b61be71a9dfcba1313a2cfb09bf19042
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2020-10-14 18:28:56 +0200

    Update index.php

--- App/index.php
+++ App/index.php
@@ -1,32 +1,5 @@
-<html>
-    <head>
-        <title>Upload PNG, JPG or PDF file to OCR.space</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js'></script>
-    </head>
-    <body>
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        <!DOCTYPE html>
+  
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
